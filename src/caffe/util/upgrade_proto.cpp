235c1b3a55e5d312fb324b770015ccb62269128f
Karl Ridgeway <kridgeway@sensoryinc.com>
2015-04-21 16:57:49 -0600

    Adding protobuf support for weighted euclidean loss layer

--- src/caffe/util/upgrade_proto.cpp
+++ src/caffe/util/upgrade_proto.cpp
@@ -925,6 +925,8 @@ const char* UpgradeV1LayerType(const V1LayerParameter_LayerType type) {
     return "Threshold";
   case V1LayerParameter_LayerType_NORMALIZE:
     return "Normalize";
+  case V1LayerParameter_LayerType_WEIGHTED_EUCLIDEAN_LOSS:
+    return "WeightedEuclideanLoss";
   default:
     LOG(FATAL) << "Unknown V1LayerParameter layer type: " << type;
     return "";
