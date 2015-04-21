b41948faff0c0a1adcbb2e57a18719a7991712bd
Karl Ridgeway <kridgeway@sensoryinc.com>
2015-04-21 17:49:03 -0600

    Excluding gpu implementation of weighted loss layer (for now)

--- include/caffe/loss_layers.hpp
+++ include/caffe/loss_layers.hpp
@@ -397,15 +397,16 @@ class WeightedEuclideanLossLayer : public LossLayer<Dtype> {
  protected:
   virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
       const vector<Blob<Dtype>*>& top);
-  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
+  /*virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top); */
 
   virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
       const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+  /*virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);*/
 
   void Apply_weights(Blob<Dtype>& blob, Blob<Dtype>& bottomWeights);
+  /*void Apply_weights_gpu(Blob<Dtype>& blob, Blob<Dtype>& bottomWeights);*/
 
   Blob<Dtype> diff_;
   Blob<Dtype> weighted_squared_diff_;
