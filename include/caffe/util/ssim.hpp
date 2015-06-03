63ad880f18e737cdfb548aa974a506e676e782b8
Karl Ridgeway <kridgeway@sensoryinc.com>
2015-06-03 11:27:43 -0600

    Using actual ssim gradient for ssim loss layer

--- include/caffe/util/ssim.hpp
+++ include/caffe/util/ssim.hpp
@@ -6,7 +6,6 @@
 typedef struct _IplImage IplImage;
 
 namespace caffe {
-
 template<typename Dtype>
 class SSIM {
 public:
@@ -14,12 +13,13 @@ public:
 
   virtual ~SSIM();
 
-  virtual void LayerSetUp(size_t x, size_t y, size_t nChan);
+  virtual void LayerSetUp(int x, int y, int nChan);
 
   virtual void Reshape(std::vector<int> shape);
 
   virtual void CalculateSSIM(const Dtype *img1_data,
-                             const Dtype *img2_data, Dtype *target);
+                             const Dtype *img2_data,
+                             Dtype *target, Dtype* target_gradient=NULL);
 
 protected:
   IplImage
@@ -28,11 +28,14 @@ protected:
     *mu1, *mu2,
     *mu1_sq, *mu2_sq, *mu1_mu2,
     *sigma1_sq, *sigma2_sq, *sigma12,
-    *ssim_map, *temp1, *temp2, *temp3;
+    *a1_a2,*b1_b2, *a1, *a2, *b1, *b2,
+    *ssim_map, *temp1, *temp2, *temp3, *gradient;
   caffe::Blob<Dtype> img1_reformatted_;
   caffe::Blob<Dtype> img2_reformatted_;
-  size_t nChan_;
-  size_t height_;
-  size_t width_;
+  int nChan_;
+  int height_;
+  int width_;
+
+  void ssimGradient();
 };
 }
--- src/caffe/util/ssim.cpp
+++ src/caffe/util/ssim.cpp
@@ -4,6 +4,8 @@
 #include <highgui.h>
 #include <vector>
 
+#define WINDOW_SIZE 11
+
 namespace caffe {
 
 template<typename Dtype>
@@ -31,7 +33,7 @@ SSIM<Dtype>::~SSIM() {
 }
 
 template<typename Dtype>
-void SSIM<Dtype>::LayerSetUp(size_t width, size_t height, size_t nChan) {
+void SSIM<Dtype>::LayerSetUp(int width, int height, int nChan) {
   height_ = height;
   width_ = width;
   nChan_ = nChan;
@@ -65,10 +67,18 @@ void SSIM<Dtype>::Reshape(std::vector<int> shape) {
   img2_reformatted_.Reshape(shape);
 }
 
+/**
+IplImage  *out  = cvCreateImage( cvSize(width,height), IPL_DEPTH_8U, 3);
+cvCvtScale(img1,out,1,0);
+cvSaveImage("img1.png",out);
+cvCvtScale(img2,out,1,0);
+cvSaveImage("img2.png",out);
+cvReleaseImage(&out);
+*/
 template<typename Dtype>
 void SSIM<Dtype>::CalculateSSIM(const Dtype *img1_data,
                                 const Dtype *img2_data,
-                                Dtype *target) {
+                                Dtype *target, Dtype* target_gradient) {
   double C1 = 6.5025, C2 = 58.5225;
   Dtype *img1_data_rfmt = img1_reformatted_.mutable_cpu_data();
   Dtype *img2_data_rfmt = img2_reformatted_.mutable_cpu_data();
@@ -88,65 +98,60 @@ void SSIM<Dtype>::CalculateSSIM(const Dtype *img1_data,
   cvSetData(img1, (void *) img1_data_rfmt, img1->widthStep);
   cvSetData(img2, (void *) img2_data_rfmt, img2->widthStep);
 
-  /**
-  IplImage  *out  = cvCreateImage( cvSize(width,height), IPL_DEPTH_8U, 3);
-  cvCvtScale(img1,out,1,0);
-  cvSaveImage("img1.png",out);
-  cvCvtScale(img2,out,1,0);
-  cvSaveImage("img2.png",out);
-  cvReleaseImage(&out);
-  */
-
   // Step 2, calculate ssim_map
   cvPow(img1, img1_sq, 2);
   cvPow(img2, img2_sq, 2);
   cvMul(img1, img2, img1_img2, 1);
 
-  cvSmooth(img1, mu1, CV_GAUSSIAN, 11, 11, 1.5);
-  cvSmooth(img2, mu2, CV_GAUSSIAN, 11, 11, 1.5);
+  cvSmooth(img1, mu1, CV_GAUSSIAN, WINDOW_SIZE, WINDOW_SIZE, 1.5);
+  cvSmooth(img2, mu2, CV_GAUSSIAN, WINDOW_SIZE, WINDOW_SIZE, 1.5);
 
   cvPow(mu1, mu1_sq, 2);
   cvPow(mu2, mu2_sq, 2);
   cvMul(mu1, mu2, mu1_mu2, 1);
 
-  cvSmooth(img1_sq, sigma1_sq, CV_GAUSSIAN, 11, 11, 1.5);
+  cvSmooth(img1_sq, sigma1_sq, CV_GAUSSIAN, WINDOW_SIZE, WINDOW_SIZE, 1.5);
   cvAddWeighted(sigma1_sq, 1, mu1_sq, -1, 0, sigma1_sq);
 
-  cvSmooth(img2_sq, sigma2_sq, CV_GAUSSIAN, 11, 11, 1.5);
+  cvSmooth(img2_sq, sigma2_sq, CV_GAUSSIAN, WINDOW_SIZE, WINDOW_SIZE, 1.5);
   cvAddWeighted(sigma2_sq, 1, mu2_sq, -1, 0, sigma2_sq);
 
-  cvSmooth(img1_img2, sigma12, CV_GAUSSIAN, 11, 11, 1.5);
+  cvSmooth(img1_img2, sigma12, CV_GAUSSIAN, WINDOW_SIZE, WINDOW_SIZE, 1.5);
   cvAddWeighted(sigma12, 1, mu1_mu2, -1, 0, sigma12);
 
-  cvScale(mu1_mu2, temp1, 2);
-  cvAddS(temp1, cvScalarAll(C1), temp1);
+  cvScale( mu1_mu2, a1, 2 );
+  cvAddS( a1, cvScalarAll(C1), a1 );
 
   // (2*sigma12 + C2)
-  cvScale(sigma12, temp2, 2);
-  cvAddS(temp2, cvScalarAll(C2), temp2);
+  cvScale( sigma12, a2, 2 );
+  cvAddS( a2, cvScalarAll(C2), a2 );
 
   // ((2*mu1_mu2 + C1).*(2*sigma12 + C2))
-  cvMul(temp1, temp2, temp3, 1);
+  cvMul( a1, a2, a1_a2, 1 );
 
   // (mu1_sq + mu2_sq + C1)
-  cvAdd(mu1_sq, mu2_sq, temp1);
-  cvAddS(temp1, cvScalarAll(C1), temp1);
+  cvAdd( mu1_sq, mu2_sq, b1 );
+  cvAddS( b1, cvScalarAll(C1), b1 );
 
   // (sigma1_sq + sigma2_sq + C2)
-  cvAdd(sigma1_sq, sigma2_sq, temp2);
-  cvAddS(temp2, cvScalarAll(C2), temp2);
+  cvAdd( sigma1_sq, sigma2_sq, b2 );
+  cvAddS( b2, cvScalarAll(C2), b2 );
 
   // ((mu1_sq + mu2_sq + C1).*(sigma1_sq + sigma2_sq + C2))
-  cvMul(temp1, temp2, temp1, 1);
+  cvMul( b1, b2, b1_b2, 1 );
 
   // ((2*mu1_mu2 + C1).*(2*sigma12 + C2))./((mu1_sq + mu2_sq + C1).*(sigma1_sq + sigma2_sq + C2))
-  cvDiv(temp3, temp1, ssim_map, 1);
+  cvDiv( a1_a2, b1_b2, ssim_map, 1 );
 
+  bool compute_gradient = target_gradient != NULL;
+  if( compute_gradient )
+    ssimGradient();
   //CvScalar index_scalar = cvAvg( ssim_map );
   //printf("SSIM r=%.2f g=%.2f b=%.2f\n", index_scalar.val[2] * 100, index_scalar.val[1] * 100, index_scalar.val[0] * 100 );
 
   // Step 3 copy ssim_map to top
   Dtype *data = (Dtype *) ssim_map->imageData;
+  Dtype *gdata = (Dtype *) gradient->imageData;
   //Dtype* target = topData + image_idx*imageSize;
   for( int chan_idx = 0; chan_idx < nChan_; chan_idx++ ) {
     for( int row_idx = 0; row_idx < height_; row_idx++) {
@@ -154,9 +159,41 @@ void SSIM<Dtype>::CalculateSSIM(const Dtype *img1_data,
         int target_idx = chan_idx * width_ * height_ + row_idx * width_ + col_idx;
         int source_idx = row_idx * width_ * nChan_ + col_idx * nChan_ + chan_idx;
         target[target_idx] = data[source_idx];
+        if( compute_gradient )
+          target_gradient[target_idx] = gdata[source_idx];
       }
     }
   }
+}
+
+template<typename Dtype>
+void SSIM<Dtype>::ssimGradient() {
+  cvMul(b2,img1,temp1);      // temp1 = b_2 * img1
+  cvMul(a2,img2,temp2);      // temp2 = a_2 * img2
+  cvSub(temp1, temp2, temp1);  // temp1 = b_2*img1 - a_2*img2
+  cvMul(a1, b1, temp2);      // temp2 = a_1 * b_1
+  cvMul(temp2, temp1, temp1 );// temp1 = a_1 * b_1( b_2 * x - a_2 * y )
+
+  //b1.*b2.*(a2-a1).*mu1 + ...
+  cvSub(a2, a1, temp2);     // temp2 = a_2 - a_1
+  cvMul(mu1, temp2, temp2);  // temp2 = mu_1 * (a_2 - a_1)
+  cvMul(b1_b2, temp2, temp2); // temp2 = b1.*b2 .* mu_1 .* (a_2 - a_1)
+  cvAdd(temp1, temp2, temp1); // temp1 = a_1 * b_1( b_2 * x - a_2 * y ) + b1.*b2 .* mu_1 .* (a_2 - a_1)
+
+  //a1.*a2.*(b1-b2).*mu2 ...
+  cvSub(b1, b2, temp2);     // temp2 = b_1 - b_2
+  cvMul(mu2, temp2, temp2);  // temp2 = (b_1-b_2).*mu_2
+  cvMul(a1_a2, temp2, temp2); // temp2 = a_1.*a_2*(b_1-b_2).*mu_2
+  cvAdd(temp1,temp2,temp1);   // temp1 = a_1 * b_1( b_2 * x - a_2 * y ) + b1.*b2 .* mu_1 .* (a_2 - a_1) + a_1.*a_2*(b_1-b_2).*mu_2
+
+  //p1 = (2 ./ ((b1.^2) .* (b2.^2) .* window_size));
+  cvPow(b1, temp2, 2.0f);                  // temp2 = b_1^2
+  cvPow(b2, temp3, 2.0f);                  // temp3 = b_2^2
+  cvMul(temp2, temp3, temp2);               // temp2 = b_1^2 * b_2^2
+  cvScale(temp2, temp2, float(WINDOW_SIZE));// temp2 = WINDOW_SIZE * (b_1^2 * b_2^2)
+  cvPow(temp2, temp2, -1.0f);               // temp2 = 1 / WINDOW_SIZE * (b_1^2 * b_2^2);
+  cvScale(temp2,temp2, 2.0f );              // temp2 = 2 / (WINDOW_SIZE * (b_1^2 * b_2^2));
+  cvMul(temp2, temp1, gradient);
 }
 
 INSTANTIATE_CLASS(SSIM);
