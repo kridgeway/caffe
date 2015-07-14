dc9d51362d767eb512a721e819f6f29544bf2e26
Karl Ridgeway <kridgeway@sensoryinc.com>
2015-07-14 15:03:55 -0600

    adding ssim optimize script

--- tools/ssim_optimize.cpp
+++ tools/ssim_optimize.cpp
@@ -6,6 +6,18 @@
 using namespace std;
 using namespace caffe;
 
+
+void printImg( IplImage* img, int channel ) {
+  float* data = (float*)img->imageData;
+  for( int col = 0; col < img->width; col++ ) {
+    for( int row = 0; row < img->height; row++ ) {
+      int idx = row * img->height * 3 + col * 3 + channel;
+      printf("%.15f ", data[idx] );
+    }
+  }
+  printf("\n");
+}
+
 int main( int argc, char** argv) {
   IplImage* img1_temp = cvLoadImage(argv[1]);
   IplImage* img2_temp = cvLoadImage(argv[2]);
@@ -15,6 +27,7 @@ int main( int argc, char** argv) {
 
   IplImage* img1 = cvCreateImage( size, d, nChan);
   IplImage* img2 = cvCreateImage( size, d, nChan);
+
   IplImage* gradient = cvCreateImage(size,d,nChan);
   IplImage* ssim_map = cvCreateImage(size,d,nChan);
 
@@ -33,15 +46,21 @@ int main( int argc, char** argv) {
                        (float *) img2->imageData,
                        (float*) ssim_map->imageData,
                        (float*) gradient->imageData, false);
-    cvScale(gradient, gradient, 50);
+    float gSum = caffe_cpu_asum(32*32*3, (float*)gradient->imageData );
+//    cvScale(gradient, gradient, 50);
     cvAdd(gradient, img2, img2);
-    if (i % 10 == 0) {
+    if (i % 100 == 0) {
+      //printImg(img2, 1);
       CvScalar index_scalar = cvAvg(ssim_map);
       cout << "(R, G & B SSIM index)" << endl;
       cout << index_scalar.val[2] * 100 << "%" << endl;
       cout << index_scalar.val[1] * 100 << "%" << endl;
       cout << index_scalar.val[0] * 100 << "%" << endl;
-      cout << caffe_cpu_asum( 32*32*3, (float*)gradient->imageData ) << endl;
+
+      cout << ((float*)img1->imageData)[0]<< " " << ((float*)img2->imageData)[0]<<endl;
+
+      cout << caffe_cpu_asum(32*32*3, (float*)img1->imageData) << " "<< caffe_cpu_asum(32*32*3,(float*)img2->imageData) << " "<<
+        gSum << endl;
     }
   }
   return 0;
